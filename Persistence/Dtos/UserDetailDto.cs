c98aa8a1aeb5d85e4c39504c7bc82818eb780947
Rencil Justin Evangelista <revangelista@hccscoding.com>
2020-02-06 11:41:47 +0800

    add DTO for authentication

---
+++ Persistence/Dtos/UserDetailDto.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace NinjaApp.Persistence.Dtos
+{
+    public class UserDetailDto
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string Email { get; set; }
+    }
+}
