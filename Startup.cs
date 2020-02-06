4347bae2bb80de81efe10a7dbb0e0fc2512a3ebd
Rencil Justin Evangelista <revangelista@hccscoding.com>
2020-02-06 12:01:35 +0800

    configure authentication and authorization

--- Startup.cs
+++ Startup.cs
@@ -1,22 +1,21 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using NinjaApp.Core;
 using NinjaApp.Core.Models;
 using NinjaApp.Core.Seeds;
 using NinjaApp.Data;
 using NinjaApp.Persistence;
+using NinjaApp.Shared.Enums;
 
 namespace NinjaApp
 {
@@ -35,6 +34,8 @@ namespace NinjaApp
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddCors();
+
             services.AddSingleton<WeatherForecastService>();
 
             services.AddDbContext<NinjaDbContext>
@@ -52,6 +53,26 @@ namespace NinjaApp
             services.AddIdentity<User, Role>()
                 .AddEntityFrameworkStores<NinjaDbContext>()
                 .AddDefaultTokenProviders();
+
+             services.AddAuthentication(opt => {
+                 opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                 opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(opt => {
+                opt.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidIssuer = Configuration["Token:Issuer"],
+                    ValidAudience = Configuration["Token:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration["Token:Key"])),
+                    ValidateIssuerSigningKey = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            services.AddAuthorization(opt => {
+                opt.AddPolicy(PolicyPrefix.RequiresAdmin, p => p.RequireRole(RolePrefix.Admin));
+                opt.AddPolicy(PolicyPrefix.RequiresModerator, p => p.RequireRole(RolePrefix.Moderator));
+                opt.AddPolicy(PolicyPrefix.RequiresUser, p => p.RequireRole(RolePrefix.User, RolePrefix.User));
+            });
             
             services.AddTransient<SeedUsersAndRoles>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -74,6 +95,13 @@ namespace NinjaApp
             }
 
             seed.BeginSeeding();
+
+            app.UseAuthentication();
+            app.UseCors(opt =>
+                opt.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+            );
 
             //app.UseHttpsRedirection();
             app.UseStaticFiles();
